544e0124410accf50f266c5e27794cdb6db2d962
Jesse Evans <jesseelizabeth1@gmail.com>
2018-10-18 23:56:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 // Code your solution in this file!
-const block = 264
+const feetInBlock = 264
 const hq = 42
 
 // someValue = 43rd street
 // hq = 42nd street
 function distanceFromHqInBlocks(currentBlock) {
   if (currentBlock > hq) {
-  return currentBlock - hq 
+    return currentBlock - hq 
   }
   else {
     return hq - currentBlock
   }
+}
+
+function distanceFromHqInFeet(currentBlock) {
+  return currentBlock * 264
 }
